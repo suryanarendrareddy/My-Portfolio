a6e5291dba9c7af5dcabf4c387c34a7aef2fe9ea
Muli surya Narendra reddy <133997432+Narendra-017@users.noreply.github.com>
2025-04-20 10:41:12 +0530

    Update Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -45,7 +45,7 @@ const projects = [
 
 const Projects = () => {
   return (
-    <div className="bg-gradient-to-br from-gray-900 to-black text-white py-16 px-6">
+    <div className="text-white py-16 px-6">
       <h1 className="text-4xl font-bold text-center text-lime-400 mb-14">
         My Projects
       </h1>
